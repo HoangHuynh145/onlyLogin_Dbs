e586fcdb3b962fc4d849aa8170e10aa1039bbd0b
HoangHuynh145 <95960229+HoangHuynh145@users.noreply.github.com>
2022-07-13 22:18:20 +0700

    add config cors

--- src/index.js
+++ src/index.js
@@ -8,12 +8,17 @@ const app = express()
 app.use(express.urlencoded({
     extended: true,
 }))
-app.use(cors({
-    credentials: true,
-    origin: true,
-}));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
+app.all("*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
 
 // connect to db
 db.connect()
