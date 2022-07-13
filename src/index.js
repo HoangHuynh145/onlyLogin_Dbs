4725d981c8f7435c49330e7fcfee515bc7ba56ea
HoangHuynh145 <95960229+HoangHuynh145@users.noreply.github.com>
2022-07-13 22:38:56 +0700

    add config cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
-// const cors = require('cors')
-const ios = require('socket.io');
+const cors = require('cors')
 const db = require('./config/db')
 const router = require('./routes')
 const app = express()
@@ -12,34 +11,26 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.json());
 
-// const whitelist = ['http://localhost:3000'];
-// const corsOptions = {
-//     credentials: true, // This is important.
-//     origin: (origin, callback) => {
-//         if (whitelist.includes(origin))
-//             return callback(null, true)
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+    credentials: true, // This is important.
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin))
+            return callback(null, true)
 
-//         callback(new Error('Not allowed by CORS'));
-//     }
-// }
-// app.use(cors(corsOptions));
-
-const io = new ios.Server({
-    allowEIO3: true,
-    cors: {
-        origin: true,
-        credentials: true
-    },
-})
+        callback(new Error('Not allowed by CORS'));
+    }
+}
+app.use(cors(corsOptions));
 
 // connect to db
 db.connect()
 
 // init route
-router(io)
+router(app)
 const port = process.env.PORT || 8000
 
 
-io.listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
