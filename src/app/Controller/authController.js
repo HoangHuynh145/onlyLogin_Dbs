0f8d1b315a48b10c6eb977c61528412994535ac9
HoangHuynh145 <95960229+HoangHuynh145@users.noreply.github.com>
2022-07-14 13:33:59 +0700

    test fix bug

--- src/app/Controller/authController.js
+++ src/app/Controller/authController.js
@@ -72,13 +72,13 @@ class AuthController {
                         const refreshToken = generateRefreshToken(user)
                         refreshTokens.push(refreshToken)
                         // add cookies
-                        const rsCk = await res.cookie('refreshToken', refreshToken, {
+                        await res.cookie('refreshToken', refreshToken, {
                             httpOnly: true,
                             secure: true,
                             path: "/",
-                            sameSite: "Strict"
+                            sameSite: "None"
                         })
-                        console.log('Cookie added', rsCk)
+                        console.log('Cookie added')
                         // tránh trả về password (._doc để parse)
                         const { password, ...others } = user._doc
                         res.status(200).json({ ...others, accessToken })
@@ -114,7 +114,7 @@ class AuthController {
                     httpOnly: true,
                     secure: true,
                     path: "/",
-                    sameSite: "Strict"
+                    sameSite: "None"
                 })
                 refreshTokens.push(newRefreshToken)
                 return res.status(200).json({ accessToken: newAccessToken })
